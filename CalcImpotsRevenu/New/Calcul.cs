8c1e194909549e70bc6e656386b864f3e5075e3e
alinddicu <alinddicu@yahoo.com>
2015-02-01 14:09:45 +0100

    small refactoring

--- CalcImpotsRevenu/New/Calcul.cs
+++ CalcImpotsRevenu/New/Calcul.cs
@@ -15,24 +15,22 @@ namespace New
 
         public ImpotResult Calculer(double revenuNet)
         {
-            return new ImpotResult(
-                CalculerMontantParTranches(revenuNet).ToArray(),
-                CalculerImpotRevenu(revenuNet));
+            var montantParTranches = CalculerMontantParTranches(revenuNet).ToArray();
+            return new ImpotResult(montantParTranches, CalculerImpotRevenu(montantParTranches));
         }
 
         private IEnumerable<double> CalculerMontantParTranches(double netImposable)
         {
-            var apresAbattement = netImposable * (1 - _parametresEtat.TauxAbattement);
+            var netApresAbattement = netImposable * (1 - _parametresEtat.TauxAbattement);
 
             return _parametresEtat.Tranches
                 .OrderBy(t => t.LimiteInf)
-                .Select(t => t.CalculerMontant(apresAbattement));
+                .Select(t => t.CalculerMontant(netApresAbattement));
         }
 
-        private double CalculerImpotRevenu(double netImposable)
+        private static double CalculerImpotRevenu(IEnumerable<double> montantParTranches)
         {
-            var impotsParTranche = CalculerMontantParTranches(netImposable);
-            return impotsParTranche.Sum(montantParTranche => montantParTranche);
+            return montantParTranches.Sum(montantParTranche => montantParTranche);
         }
     }
 }
